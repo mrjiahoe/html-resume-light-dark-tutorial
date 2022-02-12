8d12f46fa3c39c3bddaec357afe5967a7d547467
jiahoe <mrjiahoe@hotmail.com>
2022-02-12 14:11:24 +0800

    fixed active link

--- js/main.js
+++ js/main.js
@@ -34,14 +34,14 @@ function scrollActive() {
 		const sectionTop = current.offsetTop - 50;
 		sectionId = current.getAttribute("id");
 
-		if (scrollY > sectionTop <= sectionTop + sectionHeight) {
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
 			document
 				.querySelector(".nav__menu a[href*=" + sectionId + "]")
 				.classList.add("active-link");
 		} else {
 			document
 				.querySelector(".nav__menu a[href*=" + sectionId + "]")
-				.remove("active-link");
+				.classList.remove("active-link");
 		}
 	});
 }
